ceaaa306bf3f9d34b0c817a1cba4356169e62fd5
Nicolás Montiel <nicolasmontielf@gmail.com>
2021-11-18 20:09:12 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "morgan";
+import * as path from "path";
+
+import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
+
+// Routes
+import { index } from "./routes/index";
+// Create Express server
+export const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+
+app.use(logger("dev"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/", index);
+
+app.use(errorNotFoundHandler);
+app.use(errorHandler);
